f633a888ce042fee879b6a65991b5318f50911f1
al-shaimon <alshaimon968@gmail.com>
2024-11-16 23:57:22 +0600

    part-4

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -1,8 +1,19 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { userController } from './user.controller';
 
 const router = express.Router();
 
-router.post('/', userController.createAdmin);
+const auth = (...roles: string[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req.headers.authorization;
+      console.log(token);
+    } catch (err) {
+      next(err);
+    }
+  };
+};
+
+router.post('/', auth('ADMIN', 'SUPER_ADMIN'), userController.createAdmin);
 
 export const userRoutes = router;
