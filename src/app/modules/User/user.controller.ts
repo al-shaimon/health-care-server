a7572c05ee440587990f4baf672136694893c591
al-shaimon <alshaimon968@gmail.com>
2024-11-18 02:17:06 +0600

    part-5

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -1,24 +1,75 @@
 import { Request, Response } from 'express';
 import { userService } from './user.service';
+import sendResponse from '../../../shared/sendResponse';
+import catchAsync from '../../../shared/catchAsync';
+import httpStatus from 'http-status';
+import pick from '../../../shared/pick';
+import { userFilterableFields } from './user.constant';
 
-const createAdmin = async (req: Request, res: Response) => {
-  try {
-    // console.log(req.body);
-    const result = await userService.createAdmin(req.body);
-    res.status(200).json({
+const createAdmin = catchAsync(async (req: Request, res: Response) => {
+  const result = await userService.createAdmin(req);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Admin Created successfully!',
+    data: result,
+  });
+});
+
+const createDoctor = catchAsync(async (req: Request, res: Response) => {
+  const result = await userService.createDoctor(req);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Doctor Created successfully!',
+    data: result,
+  });
+});
+
+const createPatient = catchAsync(async (req: Request, res: Response) => {
+  const result = await userService.createPatient(req);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Patient Created successfully!',
+    data: result,
+  });
+});
+
+const getAllFromDB = catchAsync(async (req, res) => {
+  // console.log(req.query);
+  const filters = pick(req.query, userFilterableFields);
+  const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+
+  const result = await userService.getAllFromDB(filters, options);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Users data fetched',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
+const changeProfileStatus = catchAsync(async (req: Request, res: Response) => {
+
+  const { id } = req.params;
+  const result = await userService.changeProfileStatus(id, req.body)
+
+  sendResponse(res, {
+      statusCode: httpStatus.OK,
       success: true,
-      message: 'Admin created successfully',
-      data: result,
-    });
-  } catch (err) {
-    res.status(500).json({
-      success: false,
-      message: err?.name || 'Something went wrong',
-      error: err,
-    });
-  }
-};
+      message: "Users profile status changed!",
+      data: result
+  })
+});
+
 
 export const userController = {
   createAdmin,
+  createDoctor,
+  createPatient,
+  getAllFromDB,
+  changeProfileStatus
 };
--- src/app/modules/User/user.service.ts
+++ src/app/modules/User/user.service.ts
@@ -1,12 +1,26 @@
-import { UserRole } from '@prisma/client';
+import { Admin, Doctor, Patient, Prisma, UserRole } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
 import prisma from '../../../shared/prisma';
+import { fileUploader } from '../../../helpers/fileUploader';
+import { IFile } from '../../interfaces/file';
+import { Request } from 'express';
+import { IPaginationOptions } from '../../interfaces/pagination';
+import { paginationHelper } from '../../../helpers/paginationHelper';
+import { userSearchAbleFields } from './user.constant';
 
-const createAdmin = async (data: any) => {
-  const hashedPassword = await bcrypt.hash(data.password, 12);
+const createAdmin = async (req: Request): Promise<Admin> => {
+  const file = req.file as IFile;
+
+  if (file) {
+    const uploadToCloudinary = await fileUploader.uploadToCloudinary(file);
+
+    req.body.admin.profilePhoto = uploadToCloudinary?.secure_url;
+  }
+
+  const hashedPassword = await bcrypt.hash(req.body.password, 12);
 
   const userData = {
-    email: data.admin.email,
+    email: req.body.admin.email,
     password: hashedPassword,
     role: UserRole.ADMIN,
   };
@@ -17,7 +31,7 @@ const createAdmin = async (data: any) => {
     });
 
     const createdAdminData = await transactionClient.admin.create({
-      data: data.admin,
+      data: req.body.admin,
     });
 
     return createdAdminData;
@@ -26,8 +40,161 @@ const createAdmin = async (data: any) => {
   return result;
 };
 
+const createDoctor = async (req: Request): Promise<Doctor> => {
+  const file = req.file as IFile;
 
+  if (file) {
+    const uploadToCloudinary = await fileUploader.uploadToCloudinary(file);
+
+    req.body.doctor.profilePhoto = uploadToCloudinary?.secure_url;
+  }
+
+  const hashedPassword = await bcrypt.hash(req.body.password, 12);
+
+  const userData = {
+    email: req.body.doctor.email,
+    password: hashedPassword,
+    role: UserRole.DOCTOR,
+  };
+
+  const result = await prisma.$transaction(async (transactionClient) => {
+    await transactionClient.user.create({
+      data: userData,
+    });
+
+    const createdDoctorData = await transactionClient.doctor.create({
+      data: req.body.doctor,
+    });
+
+    return createdDoctorData;
+  });
+
+  return result;
+};
+
+const createPatient = async (req: Request): Promise<Patient> => {
+  const file = req.file as IFile;
+
+  if (file) {
+    const uploadToCloudinary = await fileUploader.uploadToCloudinary(file);
+
+    req.body.patient.profilePhoto = uploadToCloudinary?.secure_url;
+  }
+
+  const hashedPassword = await bcrypt.hash(req.body.password, 12);
+
+  const userData = {
+    email: req.body.patient.email,
+    password: hashedPassword,
+    role: UserRole.PATIENT,
+  };
+
+  const result = await prisma.$transaction(async (transactionClient) => {
+    await transactionClient.user.create({
+      data: userData,
+    });
+
+    const createdPatientData = await transactionClient.patient.create({
+      data: req.body.patient,
+    });
+
+    return createdPatientData;
+  });
+
+  return result;
+};
+
+const getAllFromDB = async (params: any, options: IPaginationOptions) => {
+  const { page, limit, skip } = paginationHelper.calculatePagination(options);
+  const { searchTerm, ...filterData } = params;
+  const andConditions: Prisma.UserWhereInput[] = [];
+
+  if (params.searchTerm) {
+    andConditions.push({
+      OR: userSearchAbleFields.map((field) => ({
+        [field]: {
+          contains: params.searchTerm,
+          mode: 'insensitive',
+        },
+      })),
+    });
+  }
+
+  if (Object.keys(filterData).length > 0) {
+    andConditions.push({
+      AND: Object.keys(filterData).map((key) => ({
+        [key]: {
+          equals: (filterData as any)[key],
+        },
+      })),
+    });
+  }
+
+  // console.dir(andConditions, { depth: 'infinity' });
+  const whereConditions: Prisma.UserWhereInput =
+    andConditions.length > 0 ? { AND: andConditions } : {};
+
+  const result = await prisma.user.findMany({
+    where: whereConditions,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? {
+            [options.sortBy]: options.sortOrder,
+          }
+        : {
+            createdAt: 'desc',
+          },
+    select: {
+      id: true,
+      email: true,
+      role: true,
+      needPasswordChange: true,
+      status: true,
+      createdAt: true,
+      updatedAt: true,
+      admin: true,
+      patient: true,
+      doctor: true,
+    },
+  });
+
+  const total = await prisma.user.count({
+    where: whereConditions,
+  });
+
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  };
+};
+
+const changeProfileStatus = async (id: string, status: UserRole) => {
+  await prisma.user.findUniqueOrThrow({
+    where: {
+      id,
+    },
+  });
+
+  const updateUserStatus = await prisma.user.update({
+    where: {
+      id,
+    },
+    data: status,
+  });
+
+  return updateUserStatus;
+};
 
 export const userService = {
   createAdmin,
+  createDoctor,
+  createPatient,
+  getAllFromDB,
+  changeProfileStatus,
 };
